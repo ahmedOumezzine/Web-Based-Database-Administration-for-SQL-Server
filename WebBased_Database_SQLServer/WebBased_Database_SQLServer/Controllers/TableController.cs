f985b058012dcd07a160704870775a96969dd016
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2018-10-15 00:36:04 +0100

    display all databases & all tables for every base

---
+++ WebBased_Database_SQLServer/WebBased_Database_SQLServer/Controllers/TableController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBased_Database_SQLServer.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult Index(String name)
+        {
+            ViewBag.database_name = name;
+            return View();
+        }
+    }
+}
---
+++ WebBased_Database_SQLServer/WebBased_Database_SQLServer/Models/Table.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBased_Database_SQLServer.Models
+{
+    public class Table
+    {
+        public string TABLE_CATALOG { get; set; }
+        public string TABLE_NAME { get; set; }
+    }
+}
