f85e364d946f2d12c91dda9d6ed379b9230b0efc
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2018-10-15 01:27:47 +0100

    display Columns

--- WebBased_Database_SQLServer/WebBased_Database_SQLServer/Controllers/TableController.cs
+++ WebBased_Database_SQLServer/WebBased_Database_SQLServer/Controllers/TableController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +16,28 @@ namespace WebBased_Database_SQLServer.Controllers
         {
             ViewBag.database_name = name;
             return View();
+        }
+
+        public ActionResult show(String name,String tablename)
+        {
+            ViewBag.database_name = name;
+            ViewBag.tablename = tablename;
+            DataSet ds = new DataSet();
+            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "SELECT * FROM [" + name+ "].[dbo].[" + tablename+ "];";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
+            }
+
+            return View(ds);
         }
     }
 }
---
+++ WebBased_Database_SQLServer/WebBased_Database_SQLServer/Models/Column.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBased_Database_SQLServer.Models
+{
+    public class Column
+    {
+        public string TABLE_CATALOG { get; set; }
+        public string TABLE_SCHEMA { get; set; }
+        public string COLUMN_NAME { get; set; }
+        public int ORDINAL_POSITION { get; set; }
+        public string IS_NULLABLE { get; set; }
+        public string DATA_TYPE { get; set; }
+
+    }
+}
