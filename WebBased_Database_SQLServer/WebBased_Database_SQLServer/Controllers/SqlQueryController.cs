eb14d9824d7e739321ee9986253c8f9b53a0ac27
AhmedOumezzine <ahmedoumezzine@outlook.fr>
2018-10-17 15:54:42 +0100

    add Sql Query Check

---
+++ WebBased_Database_SQLServer/WebBased_Database_SQLServer/Controllers/SqlQueryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace WebBased_Database_SQLServer.Controllers
+{
+    public class SqlQueryController : Controller
+    {
+        // GET: SqlQuery
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(String RequestSQL)
+        {
+            ViewBag.RequestSQL = RequestSQL;
+            List<string> error = Parse(RequestSQL);
+            ViewBag.error = error;
+            return View(error);
+        }
+
+        public List<string> Parse(string sql)
+        {
+            TSql100Parser parser = new TSql100Parser(false);
+            Microsoft.SqlServer.TransactSql.ScriptDom.TSqlFragment fragment;
+            IList<ParseError> errors;
+            fragment = parser.Parse(new StringReader(sql), out errors);
+            if (errors != null && errors.Count > 0)
+            {
+                List<string> errorList = new List<string>();
+                foreach (var error in errors)
+                {
+                    errorList.Add(error.Message);
+                }
+                return errorList;
+            }
+            return null;
+        }
+    }
+}
